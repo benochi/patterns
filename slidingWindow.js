17507fc8a9566170a57eafda86fd2a8880bea189
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-04-19 11:04:52 -0500

    Create slidingWindow.js

---
+++ slidingWindow.js
@@ -0,0 +1,38 @@
+var totalFruit = function(tree) {
+  const max = tree.length
+  const s = new Set()
+  let ans = Number.MIN_SAFE_INTEGER
+  let sum = 0
+
+  for (let i = 0; i < max; i++) {
+    const type = tree[i]
+    if (s.size < 2) {
+      s.add(type)
+      sum++
+      continue
+    }
+
+    if (s.has(type)) {
+      sum++
+    } else {
+      ans = Math.max(ans, sum)
+      let start = i - 1
+      while (start > 0) {
+        const cur = tree[start]
+        const pre = tree[start - 1]
+        if (cur === pre) {
+          start--
+        } else {
+          break
+        }
+      }
+      sum = i - start + 1
+      s.clear()
+      s.add(type)
+      s.add(tree[i - 1])
+    }
+  }
+
+  ans = Math.max(ans, sum)
+  return ans
+};
